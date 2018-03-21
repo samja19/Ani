eb7533b3162c420b3df112ea2bec260db1b96b4f
itwill <itwill@itwill-PC>
2018-03-21 15:25:26 +0900

    홈

--- ani/src/main/java/com/ani/controller/AniMController.java
+++ ani/src/main/java/com/ani/controller/AniMController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value="/aniM/")
-public class AniRegisterController {
+public class AniMController {
 
 	@RequestMapping(value="aniregister.action", method = RequestMethod.GET)
 	public String aniRegisterForm() {
